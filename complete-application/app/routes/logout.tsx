ade954566132df2c2376d277e559a49c55b3b360
RichardJECooke <RichardJECooke@pm.me>
2023-09-08 16:03:01 +0200

    Move completed app to correct folder and ignore the mysite folder

--- complete-application/app/routes/logout.tsx
+++
@@ -1,6 +0,0 @@
-import type { LoaderFunction } from "@remix-run/node"
-import { authenticator } from "~/services/auth.server";
-
-export let loader: LoaderFunction = async ({ request }) => {
-  await authenticator.logout(request, { redirectTo: "/" });
-};
